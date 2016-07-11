4e4b0b8cd23ab359cd0f46054d19de9396c79c52
David Love <bungeemonkee@gmail.com>
2016-07-11 17:23:48 -0400

    Adds enumerable methods:
    * IEnumerable<T> T.AsEnumerable<T>(T)
    * IEnumerable<T> IEnumerable<T>.Concat(IEnumerable<T>, T)

---
+++ Utilitron/Linq/EnumerableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilitron.Linq
+{
+    /// <summary>
+    /// Extensions for any <see cref="IEnumerable{T}"/>.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Adds a single item to an enumerable.
+        /// </summary>
+        /// <typeparam name="T">The type of the item to add.</typeparam>
+        /// <param name="source">The source enumerable.</param>
+        /// <param name="item">The item to add to the enumerable.</param>
+        /// <returns>A new enumerable that includes the source and the added item.</returns>
+        public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, T item)
+        {
+            return source.Concat(item.AsEnumerable());
+        }
+    }
+}
---
+++ Utilitron/ObjectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilitron
+{
+    /// <summary>
+    /// Extensions for any object.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Creates an enumerable that only includes the given item.
+        /// </summary>
+        /// <typeparam name="T">The type of the item.</typeparam>
+        /// <param name="item">The item to include in the enumerable.</param>
+        /// <returns>An enumerable that contains only the given item.</returns>
+        public static IEnumerable<T> AsEnumerable<T>(this T item)
+        {
+            return Enumerable.Repeat(item, 1);
+        }
+    }
+}
