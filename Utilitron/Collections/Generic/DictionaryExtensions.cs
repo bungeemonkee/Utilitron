ca37ddf629306e7142424edce3b6c38fcc8b6b4a
David Love <bungeemonkee@gmail.com>
2016-08-01 15:05:58 -0400

    Adds GetOrDefault extensions for dictionaries and lookups

---
+++ Utilitron/Collections/Generic/DictionaryExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utilitron.Collections.Generic
+{
+    /// <summary>
+    ///     Generic extensions for <see cref="IDictionary{TKey,TValue}" /> objects.
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        ///     Gets a value from a dictionary or the default value if the given key does not exist in the dictionary.
+        /// </summary>
+        /// <typeparam name="TKey">The dictionary key type.</typeparam>
+        /// <typeparam name="TValue">The dictionary value type.</typeparam>
+        /// <param name="dictionary">The dictionary to search.</param>
+        /// <param name="key">The key to search the dictionary for.</param>
+        /// <returns>The value for the given key or the default value for that type if the key is not found.</returns>
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            if (dictionary == null) { throw new ArgumentNullException(nameof(dictionary)); }
+
+            TValue value;
+            return dictionary.TryGetValue(key, out value)
+                ? value
+                : default(TValue);
+        }
+    }
+}
---
+++ Utilitron/Linq/LookupExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Utilitron.Linq
+{
+    /// <summary>
+    ///     Generic extensions for <see cref="ILookup{TKey, TElement}" /> objects.
+    /// </summary>
+    public static class LookupExtensions
+    {
+        /// <summary>
+        ///     Gets a value from a lookup or the default value if the given key does not exist in the lookup.
+        /// </summary>
+        /// <typeparam name="TKey">The lookup key type.</typeparam>
+        /// <typeparam name="TValue">The lookup value type.</typeparam>
+        /// <param name="lookup">The lookup to search.</param>
+        /// <param name="key">The key to search the lookup for.</param>
+        /// <returns>The value for the given key or the default value for that type if the key is not found.</returns>
+        public static IEnumerable<TValue> GetOrDefault<TKey, TValue>(this ILookup<TKey, TValue> lookup, TKey key)
+        {
+            return lookup.Contains(key)
+                       ? lookup[key]
+                       : null;
+        }
+    }
+}
