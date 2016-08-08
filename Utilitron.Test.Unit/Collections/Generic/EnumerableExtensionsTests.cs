bfd658088d17ba3dad8f966b41c6bb5ad4d5bf37
David Love <bungeemonkee@gmail.com>
2016-08-08 17:02:31 -0400

    Adds an enumerable extension to partition a source enumerable

---
+++ Utilitron.Test.Unit/Collections/Generic/EnumerableExtensionsTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utilitron.Collections.Generic;
+
+namespace Utilitron.Test.Unit.Collections.Generic
+{
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class EnumerableExtensionsTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Partition_ArgumentNullException_When_Source_Null()
+        {
+            EnumerableExtensions.Partition<int>(null, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Partition_ArgumentOutOfRangeException_When_Parititons_Less_Than_One()
+        {
+            Enumerable.Empty<int>().Partition(0);
+        }
+
+        [TestMethod]
+        public void Partition_With_Non_List()
+        {
+            Enumerable.Repeat(1, 10).Partition(5);
+        }
+
+        [TestMethod]
+        public void Partition_List()
+        {
+            var input = new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, };
+            input.Partition(5);
+        }
+    }
+}
---
+++ Utilitron/Collections/Generic/EnumerableExtensions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Utilitron.Collections.Generic
+{
+    /// <summary>
+    /// Generic enumerable extensions.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Partition an enumerable into the given number of segments.
+        /// Order of input items is not preserved across segments.
+        /// </summary>
+        /// <typeparam name="T">The type of the items in the </typeparam>
+        /// <param name="source">The initial item source.</param>
+        /// <param name="partitions">The requested number of partitions.</param>
+        /// <returns>The input items partitioned into the given number of segments.</returns>
+        public static IEnumerable<IEnumerable<T>> Partition<T>(this IEnumerable<T> source, int partitions)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (partitions < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(partitions));
+            }
+
+            var output = new List<IEnumerable<T>>(partitions);
+
+            // Initialize the internal lists to a reasonable default
+            var total = (source as ICollection)?.Count ?? partitions;
+            var length = (total / partitions) + (total % partitions != 0 ? 1 : 0);
+            for (var i = 0; i < partitions; ++i)
+            {
+                output.Add(new List<T>(length));
+            }
+
+            // Go through every item in the source
+            using (var enumerable = source.GetEnumerator())
+            {
+                for (var i = 0; enumerable.MoveNext(); ++i)
+                {
+                    ((IList<T>)output[i % partitions]).Add(enumerable.Current);
+                }
+            }
+
+            return output;
+        }
+    }
+}
