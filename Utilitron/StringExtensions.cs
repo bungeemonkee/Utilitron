839f6ec9c56f2789c28fa9e43b4abde44e7b32c2
David Love <bungeemonkee@gmail.com>
2016-08-19 10:21:50 -0400

    Adds a string extension to get the first line of a string

---
+++ Utilitron/StringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Utilitron
+{
+    public static class StringExtensions
+    {
+        private static readonly string[] Newlines = { "\r\n", "\n" };
+
+        /// <summary>
+        /// Gets just the first line of a string (after any empty lines).
+        /// </summary>
+        public static string GetFirstLine(this string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException(nameof(text));
+            }
+
+            var lines = text.Split(Newlines, StringSplitOptions.RemoveEmptyEntries);
+            return lines.Length == 0 ? null : lines[0];
+        }
+    }
+}
