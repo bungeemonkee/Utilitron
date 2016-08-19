80f8037d55ab76ed225ac722125f59dc23312211
David Love <bungeemonkee@gmail.com>
2016-08-19 10:22:23 -0400

    Adds a TextWriter extension to print exceptions

---
+++ Utilitron/IO/TextWriterExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace Utilitron.IO
+{
+    /// <summary>
+    /// Extension methods for all <see cref="TextWriter"/>s.
+    /// </summary>
+    public static class TextWriterExtensions
+    {
+        public static void WriteException(this TextWriter @out, Exception exception)
+        {
+            while (exception != null)
+            {
+                @out.WriteLine($"EXCEPTION: {exception.GetType().Name} >>> {exception.Message.GetFirstLine()}");
+                var aggregate = exception as AggregateException;
+                if (aggregate != null)
+                {
+                    @out.WriteLine("  Inner Exceptions:");
+                    foreach (var inner in aggregate.InnerExceptions)
+                    {
+                        @out.WriteLine($"  * {inner.GetType().Name} >>> {inner.Message.GetFirstLine()}");
+                    }
+                }
+                @out.WriteLine(exception.StackTrace);
+
+                // Get the next exception
+                exception = exception.InnerException;
+                if (exception == null) continue;
+                @out.WriteLine();
+                @out.WriteLine("INNER EXCEPTION >>> ------------------------------------------------------------");
+            }
+        }
+    }
+}
