602a58360ef6b4284510b55b50425cc550df49aa
David Love <bungeemonkee@gmail.com>
2016-08-23 15:52:12 -0400

    Repository enhancements
    * Add synchronous GetConnection()
    * Support GetQuery() in base types

---
+++ Utilitron.Test.Unit/Data/RepositoryAncestor2.cs
@@ -0,0 +1,11 @@
+﻿using Utilitron.Data;
+
+namespace Utilitron.Test.Unit.Data
+{
+    public class RepositoryAncestor2 : RepositoryAncestor1
+    {
+        public RepositoryAncestor2(IRepositoryConfiguration configuration) : base(configuration)
+        {
+        }
+    }
+}
---
+++ Utilitron.Test.Unit/Data/RepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Utilitron.Data;
+
+namespace Utilitron.Test.Unit.Data
+{
+    [TestClass]
+    public class RepositoryTests
+    {
+        [TestMethod]
+        public void GetQuery_Returns_Query_From_Base_Repository_Class()
+        {
+            const string expected = "QueryTest";
+
+            var configMock = new Mock<IRepositoryConfiguration>();
+
+            var repo = new RepositoryAncestor2(configMock.Object);
+
+            var result = repo.QueryTest();
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
