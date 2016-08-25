fad0f59e44f0b06b388342f74431f83ff661cfbb
David Love <bungeemonkee@gmail.com>
2016-08-25 16:04:27 -0400

    Add extension methods to produce distinct lookups

--- Utilitron/Linq/EnumerableExtensions.cs
+++ Utilitron/Linq/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Utilitron.Linq
@@ -18,6 +19,63 @@ namespace Utilitron.Linq
         public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, T item)
         {
             return source.Concat(item.AsEnumerable());
+        }
+
+        /// <summary>
+        /// Create an <see cref="ILookup{TKey,TElement}"/> with distinct element sets.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the lookup keys.</typeparam>
+        /// <typeparam name="TValue">The type of the lookup values.</typeparam>
+        /// <param name="elements">The input elements.</param>
+        /// <param name="keySelector">A function to get the keys from the input elements.</param>
+        /// <returns>An <see cref="ILookup{TKey,TElement}"/> with distinct elements.</returns>
+        public static ILookup<TKey, TValue> ToLookupDistinct<TKey, TValue>(this IEnumerable<TValue> elements, Func<TValue, TKey> keySelector)
+        {
+            return ToLookupDistinct(elements, keySelector, x => x, EqualityComparer<TKey>.Default, EqualityComparer<TValue>.Default);
+        }
+
+        /// <summary>
+        /// Create an <see cref="ILookup{TKey,TElement}"/> with distinct element sets.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the lookup keys.</typeparam>
+        /// <typeparam name="TValue">The type of the lookup values.</typeparam>
+        /// <typeparam name="TElement">The type of  the input elements.</typeparam>
+        /// <param name="elements">The input elements.</param>
+        /// <param name="keySelector">A function to get the keys from the input elements.</param>
+        /// <param name="valueSelector">A function to get the values from the input eleemnts.</param>
+        /// <returns>An <see cref="ILookup{TKey,TElement}"/> with distinct elements.</returns>
+        public static ILookup<TKey, TValue> ToLookupDistinct<TKey, TValue, TElement>(this IEnumerable<TElement> elements, Func<TElement, TKey> keySelector, Func<TElement, TValue> valueSelector)
+        {
+            return ToLookupDistinct(elements, keySelector, valueSelector, EqualityComparer<TKey>.Default, EqualityComparer<TValue>.Default);
+        }
+
+        /// <summary>
+        /// Create an <see cref="ILookup{TKey,TElement}"/> with distinct element sets.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the lookup keys.</typeparam>
+        /// <typeparam name="TValue">The type of the lookup values.</typeparam>
+        /// <typeparam name="TElement">The type of  the input elements.</typeparam>
+        /// <param name="elements">The input elements.</param>
+        /// <param name="keySelector">A function to get the keys from the input elements.</param>
+        /// <param name="valueSelector">A function to get the values from the input eleemnts.</param>
+        /// <param name="keyComparer">The comparer used for the keys.</param>
+        /// <param name="valueComparer">The comparer used for the values.</param>
+        /// <returns>An <see cref="ILookup{TKey,TElement}"/> with distinct elements.</returns>
+        public static ILookup<TKey, TValue> ToLookupDistinct<TKey, TValue, TElement>(this IEnumerable<TElement> elements, Func<TElement, TKey> keySelector, Func<TElement, TValue> valueSelector, IEqualityComparer<TKey> keyComparer, IEqualityComparer<TValue> valueComparer)
+        {
+            var result = new DistinctLookup<TKey, TValue>(keyComparer, valueComparer);
+
+            foreach (var item in elements)
+            {
+                var key = keySelector(item);
+                var value = valueSelector(item);
+
+                if (result.Contains(key, value)) continue;
+
+                result.Add(key, value);
+            }
+
+            return result;
         }
     }
 }
