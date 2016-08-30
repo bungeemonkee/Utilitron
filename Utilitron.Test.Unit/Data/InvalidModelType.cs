cce60b71883b81fc12f42b9498805887b2c127f1
David Love <bungeemonkee@gmail.com>
2016-08-30 17:08:30 -0400

    Adds Model unit tests

---
+++ Utilitron.Test.Unit/Data/InvalidModelType.cs
@@ -0,0 +1,11 @@
+﻿using Utilitron.Data;
+
+namespace Utilitron.Test.Unit.Data
+{
+    public class InvalidModelType : Model<int, ValidModelType>
+    {
+        public int Id { get; set; }
+
+        public override int GetIdentifier() => Id;
+    }
+}
---
+++ Utilitron.Test.Unit/Data/ValidModelType.cs
@@ -0,0 +1,11 @@
+﻿using Utilitron.Data;
+
+namespace Utilitron.Test.Unit.Data
+{
+    public class ValidModelType : Model<int, ValidModelType>
+    {
+        public int Id { get; set; }
+
+        public override int GetIdentifier() => Id;
+    }
+}
