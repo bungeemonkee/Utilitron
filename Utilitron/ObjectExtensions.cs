39baae7417401f1984507b2e6955b2616464fc99
David Love <bungeemonkee@gmail.com>
2016-11-07 11:56:14 -0500

    Rename AsEnumerable to InEnumerable to prevent namespace collisions

--- Utilitron/ObjectExtensions.cs
+++ Utilitron/ObjectExtensions.cs
@@ -14,7 +14,7 @@ namespace Utilitron
         /// <typeparam name="T">The type of the item.</typeparam>
         /// <param name="item">The item to include in the enumerable.</param>
         /// <returns>An enumerable that contains only the given item.</returns>
-        public static IEnumerable<T> AsEnumerable<T>(this T item)
+        public static IEnumerable<T> InEnumerable<T>(this T item)
         {
             return Enumerable.Repeat(item, 1);
         }
