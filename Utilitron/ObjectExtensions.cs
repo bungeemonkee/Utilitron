8e7d82ababab2ed9f6233da160288f32f270e9b2
David Love <bungeemonkee@gmail.com>
2016-11-07 12:00:37 -0500

    Adds a version of Concat(...) that prepends the added item

--- Utilitron/ObjectExtensions.cs
+++ Utilitron/ObjectExtensions.cs
@@ -18,5 +18,18 @@ namespace Utilitron
         {
             return Enumerable.Repeat(item, 1);
         }
+
+        /// <summary>
+        ///     Creates an enumerable that starts with this object and continues with the given enumerable.
+        ///     Effectively adds this object to the beginning of the enumerable.
+        /// </summary>
+        /// <typeparam name="T">The type of the item to add.</typeparam>
+        /// <param name="item">The item to add to the enumerable.</param>
+        /// <param name="items">The source enumerable.</param>
+        /// <returns>A new enumerable that includes the source after this item.</returns>
+        public static IEnumerable<T> Concat<T>(this T item, IEnumerable<T> items)
+        {
+            return item.InEnumerable().Concat(items);
+        }
     }
 }
