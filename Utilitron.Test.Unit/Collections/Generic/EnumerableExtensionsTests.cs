06937cac028fff5db900108fd37fb89aa96eb597
David Love <bungeemonkee@gmail.com>
2016-11-07 18:14:05 -0500

    Add some unit tests

--- Utilitron.Test.Unit/Collections/Generic/EnumerableExtensionsTests.cs
+++ Utilitron.Test.Unit/Collections/Generic/EnumerableExtensionsTests.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NcApi.Tests.Unit;
 using Utilitron.Collections.Generic;
 
 namespace Utilitron.Test.Unit.Collections.Generic
@@ -35,6 +38,108 @@ namespace Utilitron.Test.Unit.Collections.Generic
         public void Partition_With_Non_List()
         {
             Enumerable.Repeat(1, 10).Partition(5);
+        }
+
+        [TestMethod]
+        public void MinMax_Returns_Minimum_And_Maximum_Values()
+        {
+            var source = new[]
+                         {
+                             new TestObject
+                             {
+                                 TestProperty = 2
+                             },
+                             new TestObject
+                             {
+                                 TestProperty = 3
+                             },
+                             new TestObject
+                             {
+                                 TestProperty = -1
+                             }
+                         };
+
+            var selectorMock = new Mock<Func<TestObject, int>>();
+
+            Func<TestObject, int> selector = x => x.TestProperty;
+
+            var result = source.MinMax(selector);
+
+            Assert.AreEqual(-1, result.Min);
+            Assert.AreEqual(3, result.Max);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MinMax_Throws_ArgumentNullException_For_Null_Comparer()
+        {
+            var sourceMock = new Mock<IEnumerable<TestObject>>();
+            var selectorMock = new Mock<Func<TestObject, int>>();
+
+            var result = sourceMock.Object.MinMax(selectorMock.Object, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MinMax_Throws_ArgumentNullException_For_Null_Selector()
+        {
+            var sourceMock = new Mock<IEnumerable<TestObject>>();
+            var comparerMock = new Mock<IComparer<int>>();
+
+            var result = sourceMock.Object.MinMax(null, comparerMock.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void MinMax_Throws_ArgumentNullException_For_Null_Source()
+        {
+            var selectorMock = new Mock<Func<TestObject, int>>();
+            var comparerMock = new Mock<IComparer<int>>();
+
+            var result = EnumerableExtensions.MinMax(null, selectorMock.Object, comparerMock.Object);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void MinMax_Throws_InvalidOperationException_For_Empty_Source()
+        {
+            var source = Enumerable.Empty<TestObject>();
+            var selectorMock = new Mock<Func<TestObject, int>>();
+            var comparerMock = new Mock<IComparer<int>>();
+
+            var result = source.MinMax(selectorMock.Object, comparerMock.Object);
+        }
+
+        [TestMethod]
+        public void MinMax_Uses_Custom_Comparer()
+        {
+            var source = new[]
+                         {
+                             new TestObject
+                             {
+                                 TestProperty = 2
+                             },
+                             new TestObject
+                             {
+                                 TestProperty = 3
+                             },
+                             new TestObject
+                             {
+                                 TestProperty = -1
+                             }
+                         };
+
+            Func<TestObject, int> selector = x => x.TestProperty;
+
+            var comparerMock = new Mock<IComparer<int>>();
+            comparerMock.Setup(x => x.Compare(It.IsAny<int>(), It.IsAny<int>()))
+                        .Returns((int x, int y) => x - y);
+
+            var result = source.MinMax(selector, comparerMock.Object);
+
+            Assert.AreEqual(-1, result.Min);
+            Assert.AreEqual(3, result.Max);
+            comparerMock.Verify(x => x.Compare(It.IsAny<int>(), It.IsAny<int>()));
         }
     }
 }
