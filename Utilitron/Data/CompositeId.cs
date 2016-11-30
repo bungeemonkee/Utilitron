cbe44140c4b3173ead47d6e4eacfcfd96d31787b
David Love <bungeemonkee@gmail.com>
2016-11-30 15:54:56 -0500

    Minor convience funcctions to shorten creating compossit ids

---
+++ Utilitron/Data/CompositeId.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Utilitron.Data
+{
+    /// <summary>
+    /// Static methods for <see cref="CompositeId{TKey1,TKey2}"/> and <see cref="CompositeId{TKey1,TKey2,TKey3}"/> instances.
+    /// </summary>
+    public static class CompositeId
+    {
+        /// <summary>
+        /// Creates a new <see cref="CompositeId{TKey1,TKey2}"/> instance.
+        /// </summary>
+        public static CompositeId<TKey1, TKey2> Create<TKey1, TKey2>(TKey1 key1, TKey2 key2)
+            where TKey1 : struct
+            where TKey2 : struct
+        {
+            return new CompositeId<TKey1, TKey2>(key1, key2);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="CompositeId{TKey1,TKey2,TKey3}"/> instance.
+        /// </summary>
+        public static CompositeId<TKey1, TKey2, TKey3> Create<TKey1, TKey2, TKey3>(TKey1 key1, TKey2 key2, TKey3 key3)
+            where TKey1 : struct
+            where TKey2 : struct
+            where TKey3 : struct
+        {
+            return new CompositeId<TKey1, TKey2, TKey3>(key1, key2, key3);
+        }
+    }
+}
