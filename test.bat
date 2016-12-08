87a5765de72238ee350505579e5531f54904bddc
David Love <bungeemonkee@gmail.com>
2016-12-08 17:38:10 -0500

    Still trying to get OpenCover to work

--- test.bat
+++ test.bat
@@ -3,11 +3,8 @@
 if not defined configuration set configuration=Release
 
 set project=Utilitron.Tests.Unit
-set bin=%project%\bin\%configuration%\net46
-set target=%bin%\win7-x64\dotnet-test-mstest.exe
-set libs=%bin%\%project%.dll
 
-tools\OpenCover\OpenCover.Console.exe -returntargetcode -register:user -filter:"+[Utilitron]* -[Utilitron]System.*" -target:"%target%" -targetargs:"%libs%" -output:coverage.xml
+tools\OpenCover\OpenCover.Console.exe -returntargetcode -register:user -target:"dotnet.exe" -targetargs:"test %project%" -output:coverage.xml
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 tools\coveralls.io\coveralls.net.exe --opencover coverage.xml
