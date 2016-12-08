6df4a7ef4deb8568d3d3a2283ae1a90e741e9515
David Love <bungeemonkee@gmail.com>
2016-12-08 16:49:54 -0500

    Possible fix for Open Cover error

--- test.bat
+++ test.bat
@@ -7,6 +7,6 @@ set bin=%project%\bin\%configuration%\net46
 set target=%bin%\win7-x64\dotnet-test-mstest.exe
 set libs=%bin%\%project%.dll
 
-tools\OpenCover\OpenCover.Console.exe -register:user -filter:"+[Utilitron]* -[Utilitron]System.*" -target:"%target%" -targetargs:"%libs%" -output:coverage.xml
+tools\OpenCover\OpenCover.Console.exe -register:path64 -filter:"+[Utilitron]* -[Utilitron]System.*" -target:"%target%" -targetargs:"%libs%" -output:coverage.xml
 
 tools\coveralls.io\coveralls.net.exe --opencover coverage.xml
