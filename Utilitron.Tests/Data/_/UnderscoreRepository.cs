820716e17fdefe97d975624533f0d9c7c0e12354
David Love <bungeemonkee@gmail.com>
2017-06-12 12:00:55 -0400

    Handle embedded resource names with '_'s in them correctly

---
+++ Utilitron.Tests/Data/_/UnderscoreRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.CodeDom.Compiler;
+using Utilitron.Data;
+
+namespace Utilitron.Tests.Data._
+{
+    //[ExcludeFromCodeCoverage]
+    [GeneratedCode("Testing", "")] // TODO: .NET Core won't have [ExcludeFromCodeCoverage] until 2.0 so this is temporary
+    public class UnderscoreRepository : Repository
+    {
+        public UnderscoreRepository(IRepositoryConfiguration configuration) : base(configuration)
+        {
+        }
+
+        public string UnderscoreTest()
+        {
+            return GetQuery();
+        }
+    }
+}
