6ab96417a9bac389be4d46526ea2384d87e3802f
David Love <bungeemonkee@gmail.com>
2018-01-11 13:50:39 -0500

    Add a utility to find the depth of one Type in another's inheritance tree

---
+++ Utilitron/TypeUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Utilitron
+{
+    public static class TypeUtility
+    {
+        /// <summary>
+        /// Get the depth of one type in another types inheritance tree.
+        /// </summary>
+        /// <param name="subType">The type who's inheritance tree will be searched.</param>
+        /// <param name="baseType">The type to search for in the inheritance tree.</param>
+        /// <returns>How many types down in the inheritance tree of 'subType' is 'baseType' or -1 if 'subType' does not inherit from 'baseType'.</returns>
+        public static int GetTypeDepth(Type subType, Type baseType)
+        {
+            if (subType == baseType)
+                return 0;
+
+            var depth = 0;
+            while (subType != baseType)
+            {
+                depth += 1;
+                subType = subType.BaseType;
+
+                if (subType == null)
+                    return -1;
+            }
+            return depth;
+        }
+    }
+}
