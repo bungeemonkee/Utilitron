0f97c86083cb220b9a440b179130653a0821ebac
David Love <bungeemonkee@gmail.com>
2018-01-11 13:50:39 -0500

    Add a utility to find the depth of one Type in another's inheritance tree

---
+++ Utilitron.Tests/TypeUtilityTests.cs
@@ -0,0 +1,44 @@
+﻿using System.CodeDom.Compiler;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Utilitron.Tests
+{
+    [TestClass]
+    //[ExcludeFromCodeCoverage]
+    [GeneratedCode("Testing", "")] // TODO: .NET Core won't have [ExcludeFromCodeCoverage] until 2.0 so this is temporary
+    public class TypeUtilityTests
+    {
+        [TestMethod]
+        public void Types_Are_Same()
+        {
+            var subType = typeof(object);
+            var baseType = typeof(object);
+            const int expected = 0;
+
+            var result = TypeUtility.GetTypeDepth(subType, baseType);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Types_Are_Unrelated()
+        {
+            var subType = typeof(string);
+            var baseType = typeof(int);
+            const int expected = -1;
+
+            var result = TypeUtility.GetTypeDepth(subType, baseType);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Type_Distance_1()
+        {
+            var subType = typeof(string);
+            var baseType = typeof(object);
+            const int expected = 1;
+
+            var result = TypeUtility.GetTypeDepth(subType, baseType);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
